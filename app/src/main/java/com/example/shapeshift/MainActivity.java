8478bd949b4461b4e61964d08f2a24cdb2615518
K.K <100506531+KhalifasKhalifa@users.noreply.github.com>
2022-03-19 12:59:22 -0400

    Homepage animation

--- app/src/main/java/com/example/shapeshift/MainActivity.java
+++ app/src/main/java/com/example/shapeshift/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.shapeshift;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -21,7 +23,11 @@ public class MainActivity<val> extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-
+        ConstraintLayout constraintLayout = findViewById(R.id.mainLayout);
+        AnimationDrawable animationDrawable = (AnimationDrawable) constraintLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(2500);
+        animationDrawable.setExitFadeDuration(5000);
+        animationDrawable.start();
 
         Button about = (Button) findViewById(R.id.about_button);
         about.setOnClickListener(new View.OnClickListener() {
