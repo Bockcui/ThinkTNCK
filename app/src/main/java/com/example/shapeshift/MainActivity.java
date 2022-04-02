a95d6c364481bbdc3de086bcb66df0a79a8ef977
K.K <100506531+KhalifasKhalifa@users.noreply.github.com>
2022-04-02 01:16:19 -0400

    New homepage

--- app/src/main/java/com/example/shapeshift/MainActivity.java
+++ app/src/main/java/com/example/shapeshift/MainActivity.java
@@ -23,11 +23,11 @@ public class MainActivity<val> extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        ConstraintLayout constraintLayout = findViewById(R.id.mainLayout);
-        AnimationDrawable animationDrawable = (AnimationDrawable) constraintLayout.getBackground();
-        animationDrawable.setEnterFadeDuration(2500);
-        animationDrawable.setExitFadeDuration(5000);
-        animationDrawable.start();
+//        ConstraintLayout constraintLayout = findViewById(R.id.mainLayout);
+//        AnimationDrawable animationDrawable = (AnimationDrawable) constraintLayout.getBackground();
+//        animationDrawable.setEnterFadeDuration(2500);
+//        animationDrawable.setExitFadeDuration(5000);
+//        animationDrawable.start();
 
 
         Button about = (Button) findViewById(R.id.about_button);
