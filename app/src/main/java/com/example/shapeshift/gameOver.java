fcc172cd3862824666f5a86e8e58ac4a655c7d30
Bockcui <cttcui@gmail.com>
2022-04-02 19:38:13 -0400

    file reading

--- app/src/main/java/com/example/shapeshift/gameOver.java
+++ app/src/main/java/com/example/shapeshift/gameOver.java
@@ -1,17 +1,26 @@
 package com.example.shapeshift;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
 public class gameOver extends AppCompatActivity {
+    ArrayList<Highscore> top5;
 
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
@@ -28,12 +37,26 @@ public class gameOver extends AppCompatActivity {
             }
         });
 
-Intent intentEnd= getIntent();
-int score=intentEnd.getIntExtra("val",0);
+        Intent intentEnd= getIntent();
+        int score=intentEnd.getIntExtra("val",0);
 
-scoreText.setText("Score: "+score);
+        scoreText.setText("Score: "+score);
 
+        try {
+            BufferedReader b = new BufferedReader(new FileReader("highscores.csv"));
+            String l = "";
+            String schar = ",";
+            String[] highscore;
+            while ((l = b.readLine()) != null)
+            {
+                highscore = l.split(schar);
+                top5.add(new Highscore(highscore[0], highscore[1]));
+            }
+            Collections.sort(top5, Comparator.comparing(Highscore::getScore).reversed());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-
-
 }
